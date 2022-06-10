ec33902415f6ec47d74935f688d8965a08255ffc
Zbynek Dusatko <zzdobrusky@gmail.com>
2022-06-09 21:25:21 -0600

    upgraded packages

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'build'),
+    static: path.join(__dirname, 'build'),
     compress: true,
     port: 8080,
   },
@@ -51,8 +51,8 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'assets/**/*'),
-          to: path.resolve(__dirname, 'build')
+          from: path.resolve(__dirname, 'assets'),
+          to: path.resolve(__dirname, 'build/assets')
         }
       ],
     })
